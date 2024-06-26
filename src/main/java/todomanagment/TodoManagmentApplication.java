35ac36f7de63c765a26b6a9d7a12deba47443daf
milad <baranimilad70@yahoo.com>
2024-06-26 11:15:07 +0330

    #1 - package and configs

---
+++ src/main/java/todomanagment/TodoManagmentApplication.java
@@ -0,0 +1,13 @@
+package todomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagmentApplication.class, args);
+    }
+
+}
