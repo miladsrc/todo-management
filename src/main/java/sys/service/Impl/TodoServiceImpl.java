6634853905051d95e279255d119fb7e347f7e745
milad <baranimilad70@yahoo.com>
2024-07-10 11:17:02 +0330

    #30 - Model mapper third party added to the project

--- src/main/java/sys/service/Impl/TodoServiceImpl.java
+++ src/main/java/sys/service/Impl/TodoServiceImpl.java
@@ -1,6 +1,7 @@
 package sys.service.Impl;
 
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import sys.dto.TodoDto;
 import sys.entity.Todo;
@@ -12,26 +13,30 @@ import sys.service.TodoService;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         //convert todoDto to todoJpa entity obj
-        Todo todo = Todo.builder()
-                .title(todoDto.getTitle())
-                .completed(todoDto.isCompleted())
-                .description(todoDto.getDescription())
-                .build();
+//        Todo todo = Todo.builder()
+//                .title(todoDto.getTitle())
+//                .completed(todoDto.isCompleted())
+//                .description(todoDto.getDescription())
+//                .build();
+
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
         //todo jpa entity
         Todo todoSaved = todoRepository.save(todo);
 
         //convert saved todo to todo dto entity obj
-        TodoDto todoDtoTwo = new TodoDto();
-        todoDtoTwo.setId(todoSaved.getId());
-        todoDtoTwo.setTitle(todoSaved.getTitle());
-        todoDtoTwo.setDescription(todoSaved.getDescription());
-        todoDtoTwo.setCompleted(todoSaved.isCompleted());
+//        TodoDto todoDtoTwo = new TodoDto();
+//        todoDtoTwo.setId(todoSaved.getId());
+//        todoDtoTwo.setTitle(todoSaved.getTitle());
+//        todoDtoTwo.setDescription(todoSaved.getDescription());
+//        todoDtoTwo.setCompleted(todoSaved.isCompleted());
+        TodoDto todoDtoTwo = modelMapper.map(todoSaved, TodoDto.class);
 
 
         return todoDtoTwo;
