f0096616be97ed601f5d19faa582a2b68b41714d
milad <baranimilad70@yahoo.com>
2024-07-14 11:29:47 +0330

    #45 - undrestanding role based authorization

--- src/main/java/sys/config/SpringSecurityConfig.java
+++ src/main/java/sys/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package sys.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAnyRole;
 
 /* whenever we use @Configuration
  we set spring bean configuration
@@ -35,14 +38,19 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest()
-                                .authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests((authorizeRequests) -> {
+                    authorizeRequests.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.PATCH, "/api/**"). hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+                    authorizeRequests.anyRequest().authenticated();
+                        }
+                ).httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 
@@ -50,14 +58,14 @@ public class SpringSecurityConfig {
     public UserDetailsService userDetailsService(){
 
         UserDetails user = User.builder()
-                .username("user")
+                .username("javad")
                 .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
 
 
         UserDetails admin = User.builder()
-                .username("admin")
+                .username("milad")
                 .password(passwordEncoder().encode("password"))
                 .roles("ADMIN")
                 .build();
