91ee2da65482cbb6a6cf86cf40a6dcc124d5ca7e
milad <baranimilad70@yahoo.com>
2024-07-14 19:15:26 +0330

    #50-51 - creating CustomUserDetailService class

---
+++ src/main/java/sys/security/CustomUserDetailService.java
@@ -0,0 +1,39 @@
+package sys.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sys.entity.User;
+import sys.repository.UserRepository;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(()-> new UsernameNotFoundException("user not exist by username or email"));
+
+        Set<SimpleGrantedAuthority> authoritySet = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authoritySet
+        );
+    }
+}
